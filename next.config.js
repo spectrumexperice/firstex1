d32d3d72b404542d776c7cbc897599186bca1168
spectrumexperice <spectrumweb22@gmail.com>
2025-09-09 20:09:13 +0300

    fix react modules and build process

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'standalone', // جرب 'standalone' بدل 'export'
+  trailingSlash: true,
   images: {
-    domains: ["res.cloudinary.com"],
+    unoptimized: true
   },
-};
+  reactStrictMode: true
+}
 
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin();
-
-export default withNextIntl(nextConfig);
+module.exports = nextConfig
