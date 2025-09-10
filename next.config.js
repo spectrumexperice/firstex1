a82d5a449ba255580222a42e19d529815ccb912a
spectrumexperice <spectrumweb22@gmail.com>
2025-09-10 18:26:50 +0300

    update

--- next.config.js
+++ next.config.js
@@ -4,10 +4,9 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  trailingSlash: true,
 };
 
 import createNextIntlPlugin from 'next-intl/plugin';
-
 const withNextIntl = createNextIntlPlugin();
-
 export default withNextIntl(nextConfig);
